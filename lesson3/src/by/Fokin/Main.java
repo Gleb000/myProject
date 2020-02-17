a17cb8044e42598f9341b40be5a6c04008caca04
Gleb <glebfox3@gmail.com>
2020-02-17 21:32:31 +0300

    First Commit

---
+++ lesson3/src/by/Fokin/Main.java
@@ -0,0 +1,15 @@
+package by.Fokin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Lesson3!!!");
+        int i = 1;
+        while (i < 11) {
+            System.out.println(Integer.toBinaryString(i));
+            i++;
+        }
+        //
+        //
+    }
+}
